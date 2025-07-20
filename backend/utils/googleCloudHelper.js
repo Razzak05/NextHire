8a3fe3db630d8086490955cf918eaf19b9154c36
Razzak05 <razzakchoudhary9619@gmail.com>
2025-07-20 09:22:33 +0530

    Save current changes before cleaning history

--- backend/utils/googleCloudHelper.js
+++ backend/utils/googleCloudHelper.js
@@ -4,7 +4,7 @@ import streamifier from "streamifier";
 // Initialize Google Cloud Storage
 const storage = new Storage({
   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
-  credentials: JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS),
+  credentials: JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS_JSON),
 });
 
 const bucket = storage.bucket(process.env.GOOGLE_CLOUD_BUCKET_NAME);
