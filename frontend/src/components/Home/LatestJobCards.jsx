b731a4d76f75cb0e4a1d8f32e6baa2438f1a0a73
Razzak05 <razzakchoudhary9619@gmail.com>
2025-08-02 13:09:47 +0530

    styling changed

--- frontend/src/components/Home/LatestJobCards.jsx
+++ frontend/src/components/Home/LatestJobCards.jsx
@@ -10,20 +10,22 @@ const LatestJobCards = ({ job }) => {
       onClick={() => navigate(`/description/${job._id}`)}
       className="p-5 rounded-md shadow-xl bg-white border-gray-100 cursor-pointer"
     >
-      <div>
-        <Avatar className="w-12 h-12">
-          <AvatarImage
-            src={
-              job.company?.logo?.url ||
-              "https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"
-            }
-            alt="Company Logo"
-          />
-        </Avatar>
-      </div>
-      <div>
-        <h1 className="font-medium text-lg">{job.company.name}</h1>
-        <p className="text-sm text-gray-500">{job.location}</p>
+      <div className="flex gap-2">
+        <div>
+          <Avatar className="w-12 h-12">
+            <AvatarImage
+              src={
+                job.company?.logo?.url ||
+                "https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"
+              }
+              alt="Company Logo"
+            />
+          </Avatar>
+        </div>
+        <div>
+          <h1 className="font-medium text-lg">{job.company.name}</h1>
+          <p className="text-sm text-gray-500">{job.location}</p>
+        </div>
       </div>
       <div>
         <h1 className="font-bold text-lg my-2">{job.title}</h1>
